d1b30638b6175e2ee12194795cc179d3a76362bf
Matthias Bernstein <matthias.bernstein@teamapps.org>
2023-02-02 23:45:06 +0100

    change maven central deploy server

--- src/main/java/org/teamapps/api/generator/TestMessageProtocolGeneratorMojo.java
+++ src/main/java/org/teamapps/api/generator/TestMessageProtocolGeneratorMojo.java
@@ -1,8 +1,8 @@
 /*-
  * ========================LICENSE_START=================================
- * TeamApps Protocol Schema Maven Plugin
+ * TeamApps Message Protocol Maven Plugin
  * ---
- * Copyright (C) 2022 TeamApps.org
+ * Copyright (C) 2022 - 2023 TeamApps.org
  * ---
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
